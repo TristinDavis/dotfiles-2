2b0dd04ec49748249a28ea85f447fc057bad931f
wata <w.nagasawa@gmail.com>
2013-09-02 20:29:45 +0900

    fixed README

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-Install
+Setup
 =======
 
 Following next step to use this configration files.
 
-    $ git clone git://github.com/wata/dotfiles.git ~/.dotfiles
-    $ cd ~/.dotfiles
+    $ git clone git://github.com/wata/dotfiles.git /path/to/MY_DROPBOX_DIR/dotfiles
+    $ cd /path/to/MY_DROPBOX_DIR/dotfiles
     $ ./setup.sh
+
+Setup Sublime Text 3
+=======
+
+Install Sublime Text 3 http://www.sublimetext.com/3
+
+    $ cd /path/to/MY_DROPBOX_DIR/dotfiles/sublime_text_3
+    $ ./setup_sublimetext.sh
--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 echo "setup for $HOME"
 
-ln -fs "$PWD/.bashrc"           ~/.bashrc
-ln -fs "$PWD/.bash_profile"     ~/.bash_profile
-ln -fs "$PWD/.emacs.d"          ~/.emacs.d
-ln -fs "$PWD/.gitconfig"        ~/.gitconfig
-ln -fs "$PWD/.gitignore"        ~/.gitignore
-ln -fs "$PWD/.perltidyrc"       ~/.perltidyrc
-ln -fs "$PWD/.ssh"              ~/.ssh
-ln -fs "$PWD/.chef"             ~/.chef
-ln -fs "$PWD/.uncrustifyconfig" ~/.uncrustifyconfig
+ln -fs "$PWD/.bashrc"           $HOME/.bashrc
+ln -fs "$PWD/.bash_profile"     $HOME/.bash_profile
+ln -fs "$PWD/.emacs.d"          $HOME/.emacs.d
+ln -fs "$PWD/.gitconfig"        $HOME/.gitconfig
+ln -fs "$PWD/.gitignore"        $HOME/.gitignore
+ln -fs "$PWD/.perltidyrc"       $HOME/.perltidyrc
+ln -fs "$PWD/.ssh"              $HOME/.ssh
+ln -fs "$PWD/.chef"             $HOME/.chef
+ln -fs "$PWD/.uncrustifyconfig" $HOME/.uncrustifyconfig
 
 git submodule init && git submodule update
 
-mkdir -p ~/bin
-ln -fs "$PWD/mv2trash"                 ~/bin/
-ln -fs "$PWD/git-wata"                 ~/bin/
-ln -fs "$PWD/create_secure_passwd.php" ~/bin/
+mkdir -p $HOME/bin
+ln -fs "$PWD/mv2trash"                 $HOME/bin/
+ln -fs "$PWD/git-wata"                 $HOME/bin/
+ln -fs "$PWD/create_secure_passwd.php" $HOME/bin/
 
 echo "done."
